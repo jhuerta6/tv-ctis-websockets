793ef06570e4959983825feea63b7b4a991b2e93
ulises <jorgehuertahu@gmail.com>
2018-06-07 10:34:41 -0600

    works on localhost

---
+++ app.js
@@ -0,0 +1,16 @@
+const websocket = require('ws');
+
+const wss = new websocket.Server({port: 1969});
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message){
+        console.log('received: %s', message);
+        ws.send('I received your message: ' +  message);
+        wss.clients.forEach(function each(client){
+           if(client !== ws && client.readyState === websocket.OPEN){
+               client.send("Incoming message: "+message );
+           }
+        });
+    });
+});
+

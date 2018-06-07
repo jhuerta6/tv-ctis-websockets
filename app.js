add3a0bf817b7be0b4cb7b00ebdfc514cce045a5
ulises <jorgehuertahu@gmail.com>
2018-06-07 11:16:23 -0600

    more changes; experimenting

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const websocket = require('ws');
 
-const wss = new websocket.Server({port: 80});
+const wss = new websocket.Server({port: 8080});
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message){
